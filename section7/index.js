ccf6b615f12236823c77608e31d6b42731387134
coffebean19 <istvan.mayer1999@protonmail.com>
2024-04-17 15:06:26 +0200

    Section 7p80.

--- section7/index.js
+++ section7/index.js
@@ -4,28 +4,41 @@ const PORT = 3000;
 
 const server = http.createServer();
 
+const friends = [
+  {
+    id: 0,
+    name: "Rolande",
+  },
+  {
+    id: 1,
+    name: "Damian",
+  },
+  {
+    id: 2,
+    name: "Johan",
+  },
+];
+
 server.on("request", (req, res) => {
-  if (req.url === "/friends") {
-    // res.writeHead(200, {
-    //   "Content-type": "application/json",
-    // });
+  const items = req.url.split("/");
+  if (req.method === "POST" && items[1] === "friends") {
+    req.on("data", (data) => {
+      const friend = data.toString();
+      console.log("Request: ", friend);
+      friends.push(JSON.parse(friend));
+    })
+  } else if (req.method === "GET" && items[1] === "friends") {
     res.statusCode = 200;
-    res.setHeader("Content-Type", "application/json")
-    res.end(JSON.stringify({
-      id: 1,
-      name: "Michael Jackson"
-    }));
-  } else if (req.url === "/messages") {
-    res.write("<html>");
-    res.write("<body>");
-    res.write("<ul>");
-    res.write("<li>Yow</li>");
-    res.write("<li>NewYow</li>");
-    res.write("<ul>");
-    res.write("</ul>");
-    res.write("</body>");
-    res.write("</html>");
-    res.end();
+    res.setHeader("Content-Type", "application/json");
+    if (items.length === 3) {
+      const friendIndex = Number(items[2]);
+      res.end(JSON.stringify(friends[friendIndex]));
+    } else {
+      res.end(JSON.stringify(friends));
+    }
+  } else if (req.method === "GET" && items[1] === "messages") {
+    res.setHeader("Content-Type", "text/html");
+    res.write("<p>pop</p>")
   } else {
     res.statusCode = 404;
     res.end();
