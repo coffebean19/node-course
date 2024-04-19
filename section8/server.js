3271541e27661f4c50c7615d968ed5c4b3014d50
Istvan <istvan.mayer1999@protonmail.com>
2024-04-19 16:03:13 +0200

    Section 8p88.

--- section8/server.js
+++ section8/server.js
@@ -1,12 +1,42 @@
 const express = require("express");
 
-const server = express();
+const app = express();
 
 const PORT = 3000;
 
-server.listen();
-console.log("Server listening on port: 3000..");
+const friends = [
+  {
+    id: 0,
+    name: "rolande"
+  },
+  {
+    id: 1,
+    name: "damian"
+  }
+];
 
-server.get("/", () => {
-  server.send("hello");
+app.get("/friends", (req, res) => {
+  res.json(friends);
 })
+
+app.get("/friends/:friendId", (req, res) => {
+  const friendId = Number(req.params.friendId);
+  const friend = friends[friendId];
+  if (friend) {
+    res.status(200).json(friend);
+  } else {
+    res.status(404).json({ error: 404, message: "Friend not found" });
+  }
+})
+
+app.get("/messages", (req, res) => {
+  res.send("<ul><li>Helloo</li></ul>");
+});
+
+app.post("/messages", (req, res) => {
+  console.log("Updating messages.");
+});
+
+app.listen(PORT, () => {
+  console.log(`Listening on ${PORT}...`);
+});
