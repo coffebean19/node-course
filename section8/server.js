f461c4ce4f9d4863988560f2177ce45736789efe
coffebean19 <istvan.mayer1999@protonmail.com>
2024-04-22 22:31:38 +0200

    asdkfj.

--- section8/server.js
+++ section8/server.js
@@ -15,9 +15,38 @@ const friends = [
   }
 ];
 
+app.use((req, res, next) => {
+  const start = Date.now()
+  next();
+  const delta = Date.now() - start;
+  console.log(`${req.method} ${req.url} ${delta}ms`);
+});
+
+app.use(express.json())
+
+app.post("/friends", (req, res) => {
+  if (!req.body.name) {
+    res.status(400).json({
+      error: "Missing friend name"
+    });
+  }
+
+  const newFriend = {
+    id: (friends.length),
+    name: req.body.name
+  }
+
+  friends.push(newFriend);
+
+  res.json(newFriend);
+  // const friendName = req.body.name;
+  // const friendId = friends.length;
+  // friends.push({ id: friendId, name: friendName });
+});
+
 app.get("/friends", (req, res) => {
   res.json(friends);
-})
+});
 
 app.get("/friends/:friendId", (req, res) => {
   const friendId = Number(req.params.friendId);
@@ -27,7 +56,7 @@ app.get("/friends/:friendId", (req, res) => {
   } else {
     res.status(404).json({ error: 404, message: "Friend not found" });
   }
-})
+});
 
 app.get("/messages", (req, res) => {
   res.send("<ul><li>Helloo</li></ul>");
